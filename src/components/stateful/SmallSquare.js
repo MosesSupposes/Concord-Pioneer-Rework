47b95705184396cfe808a85fe687e5d2bf610dda
MosesSupposes <mosamuel98@gmail.com>
2019-03-09 19:57:21 -0800

    add first ad component (SmallSquare)

---
+++ src/components/stateful/SmallSquare.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+
+const styles = {
+    div: {
+        display: 'inline-block',
+    },
+
+    img: {
+        width: '200px',
+        height: '200px'
+    }
+}
+
+class SmallSquare extends Component {
+    componentDidMount() {
+        const { storeMetaInfo, id, date, description, company} = this.props
+        storeMetaInfo({ 
+            type: 'small square',
+            id,
+            description,
+            date,
+            company
+        })
+    }
+
+    render() {
+        const { image, description } = this.props
+        return (
+            <div style={styles.div} >
+                <img style={styles.img} alt={description} src={image} />
+            </div>
+        )
+    }
+}
+
+export default SmallSquare
